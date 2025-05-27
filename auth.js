dadd2f79f9b72dfa50bd9e82cc17df1c29bc676e
Promit Nag <166518100+GamerPROMzy@users.noreply.github.com>
2025-05-27 23:31:29 +0530

    auth.js

---
+++ auth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3alKmHqnG0_tMl-1O7-BfShRA96RuSUM",
+  authDomain: "noteninja-71326.firebaseapp.com",
+  projectId: "noteninja-71326",
+  storageBucket: "noteninja-71326.firebasestorage.app",
+  messagingSenderId: "636549547943",
+  appId: "1:636549547943:web:322a916aecb9ce12c1d882"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
